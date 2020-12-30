603c1b73189ce973dc68148151a7725f2eebe415
kjrm209 <61448722+kjrm209@users.noreply.github.com>
2020-12-30 10:56:04 +0800

    30-12-2020 @ 10:55 AM: w3s-c2s-tut-head-script.js
    This is an initial commit.

---
+++ w3s-c2s-tut-head-script.js
@@ -0,0 +1,7 @@
+/* Date: 30 December 2020.
+w3s-c2s-tut-head-script.js
+https://kjrm209.github.io/w3schools-css-tutorial/w3s-c2s-tut-head-script.js
+//
+W3Schools - CSS - CSS Tutorials
+https://www.w3schools.com/css/default.asp
+*/
